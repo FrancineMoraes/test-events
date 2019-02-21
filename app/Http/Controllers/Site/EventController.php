4c7d01c1b3269600edd3476b2277e34807c1965c
FrancineMoraes <francinemoraes08@hotmail.com>
2019-02-21 19:31:42 -0300

    criando métodos das controllers

--- app/Http/Controllers/Site/EventController.php
+++ app/Http/Controllers/Site/EventController.php
@@ -7,5 +7,16 @@ use App\Http\Controllers\Controller;
 
 class EventController extends Controller
 {
-    //
+    private $event;
+
+    public function __construct(){
+        $this->event = new EventRepository();
+    }
+
+    // public function show($id)
+    // {
+    //     $events = $this->event->show($id);
+
+    //     return view('site.events.show', compact('event'));
+    // }
 }
--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Site;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\EventRepository;
 
 class HomeController extends Controller
 {
-    //
+    private $event;
+
+    public function __construct(){
+        $this->event = new EventRepository();
+    }
+
+    public function index()
+    {
+        $events = $this->event->showAll();
+
+        return view('site.layouts.app', compact('events'));
+    }
 }
