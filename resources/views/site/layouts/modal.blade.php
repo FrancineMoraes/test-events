c250d3ec0df980708ee3c676c1ffc2d3db57e914
FrancineMoraes <francinemoraes08@hotmail.com>
2019-02-26 22:16:03 -0300

    alterações no layout e criação da modal de pedido a ser processado

---
+++ resources/views/site/layouts/modal.blade.php
@@ -0,0 +1,18 @@
+<div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Pedido</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Seu pedido será processado.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary button-ok" onclick="redirect(interval)">Ok</button>
+            </div>
+        </div>
+    </div>
+</div>
